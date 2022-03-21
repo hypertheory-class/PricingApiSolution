625de29ff9f85bec2d989f6cd298c5127f39f783
Jeffry Gonzalez <jeff@hypertheory.com>
2022-03-21 15:56:04 -0400

    first test written

---
+++ PricingApi.IntegrationTests/GetPricingTests.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Threading.Tasks;
+using Alba;
+using Xunit;
+
+namespace PricingApi.IntegrationTests;
+public class GetPricingTests : IClassFixture<WebAppFixture>
+{
+
+    private readonly IAlbaHost _host;
+
+    public GetPricingTests(WebAppFixture fixture)
+    {
+        _host = fixture.AlbaHost;
+    }
+
+    [Fact]
+    public async Task CanGetThePricingInformation()
+    {
+        await _host.Scenario(api =>
+        {
+            api.Get.Url("/pricing");
+            api.StatusCodeShouldBeOk();
+        });
+
+    }
+}
