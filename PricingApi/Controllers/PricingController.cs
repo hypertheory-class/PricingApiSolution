46e114a94d8e0ab7f40c941c10d8934d021e032b
ITUStudent <ITUStudent@ML-RefVm-594359>
2022-03-21 15:28:39 -0400

    Add project files.

---
+++ PricingApi/Controllers/PricingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PricingApi.Controllers;
+
+public class PricingController : ControllerBase
+{
+
+    private readonly ILogger<PricingController> _logger;
+
+    public PricingController(ILogger<PricingController> logger)
+    {
+        _logger = logger;
+    }
+
+
+
+    // GET /pricing
+    [HttpGet("/pricing")]
+    public async Task<ActionResult> GetCurrentPricingAsync()
+    {
+        var response = new CurrentPricingResponse(58, 10, 35, 40);
+        _logger.LogInformation("Got a request for the current pricing " + response);
+        return Ok(response); // Return a 200 response.
+    }
+
+}
+
+
+
+public record CurrentPricingResponse(decimal baseAmount, decimal waterDaily, decimal electicalDaily, decimal lakeFrontDaily);
---
+++ PricingApi/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args); // our configuration
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run(); // this is a blocking call that starts a web server and waits for requests. (Kestrel)
